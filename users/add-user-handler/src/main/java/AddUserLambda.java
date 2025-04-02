426f8fd33fc32f2fdd92ba6a3c8149ef5d8155a6
dilarakilic <dilaraaklc@gmail.com>
2025-04-02 18:33:02 +0300

    Refactor project structure

--- users/add-user-handler/src/main/java/AddUserLambda.java
+++ users/add-user-handler/src/main/java/AddUserLambda.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
--- users/update-user-handler/src/main/java/UpdateUserLambda.java
+++ users/update-user-handler/src/main/java/UpdateUserLambda.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.JsonNode;
